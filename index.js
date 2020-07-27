2d07e7aa6f4c88aafa1536c06246bfe8b8cae91b
oncomouse <oncomouse@gmail.com>
2020-07-27 18:07:07 -0500

    Correct array method

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
+// Based on: https://stackoverflow.com/questions/31768457/idiomatic-way-to-mutate-a-property-with-multiple-events-using-kefir
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     define(['kefir'], factory);
   } else if (typeof module === 'object' && module.exports) {
     module.exports = factory(require('kefir'));
   } else {
-    root.returnExports = factory(root.Kefir);
+    factory(root.Kefir);
   }
 }(typeof self !== 'undefined' ? self : this, function (Kefir) {
-  return function () {
+  Kefir.update = function () {
     var initValue = arguments.slice(0, 1)
     var args = arguments.slice(1)
-    var mutations = [];
-    args.reduce(function (mutations, arg) {
-      var sources = arg.slice(0, -1)
-      var newValue = arg.slice(-1)
-      return mutations.append(Kefir.zip(sources).map(e => ({event: e, mutation: newValue})))
-    }, [])
+    var mutations = args
+      .reduce(function (mutations, arg) {
+        var sources = arg.slice(0, -1)
+        var newValue = arg.slice(-1)
+        return mutations.append(Kefir.zip(sources).map(e => ({event: e, mutation: newValue})))
+      }, [])
 
     return kefir
       .merge(mutations)
       .scan
-      (function (prev, {event, mutation}) {return mutation.apply(undefined, [prev].append(event)), initValue});
+      (function (prev, {event, mutation}) {return mutation.apply(undefined, [prev].concat(event)), initValue});
   }
 }));
