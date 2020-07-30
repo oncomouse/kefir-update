8781a44c51d173268a5c896103c3976f1b910638
oncomouse <oncomouse@gmail.com>
2020-07-30 10:23:04 -0500

    Updated

--- index.js
+++ index.js
@@ -4,25 +4,26 @@ const asFunction = f => (typeof f === 'function') ? f : () => f
 const isArray = xs => xs instanceof Array
 const isRawPattern = xs => isArray(xs[0])
 function extractPattern(pattern) {
-  if(!isArray(pattern)) {
+  if (!isArray(pattern)) {
     return Kefir.never
   }
-  if(isRawPattern(pattern)) {
+  if (isRawPattern(pattern)) {
     return pattern
   }
-  return [pattern.slice(0,-1), asFunction(pattern.slice(-1)[0])]
+  return [pattern.slice(0, -1), asFunction(pattern.slice(-1)[0])]
 }
 const append = (...args) => [...args]
-Kefir.update = function(initValue, ...patterns) {
-  return Kefir.merge(patterns
-    .map(extractPattern)
-    .map(([sources, f]) => Kefir.combine(sources, append).map(e => ({
-      event: e,
-      mutation: f
-    })))
+Kefir.update = function (initValue, ...patterns) {
+  return Kefir.merge(
+    patterns
+      .map(extractPattern)
+      .map(([sources, f]) => Kefir.combine(sources, append).map(e => ({
+        event: e,
+        mutation: f
+      })))
   )
-  .scan(
-    (prev, {event, mutation}) => mutation.apply(undefined, [prev].concat(event)),
-    initValue
-  )
+    .scan(
+      (prev, {event, mutation}) => mutation.apply(undefined, [prev, ...event]),
+      initValue
+    )
 }
